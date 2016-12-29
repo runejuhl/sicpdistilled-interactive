a6f40aa1cbd00ee05b38a5b9ef8879338a3ce3b6
Rune Juhl Jacobsen <runejuhl@petardo.dk>
2016-12-29 21:38:40 +0100

    Remove s3 and CDN from config, add http prefix for Github pages

--- config.rb
+++ config.rb
@@ -17,24 +17,10 @@ configure :development do
 end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
-
-activate :s3_sync do |s3_sync|
-  s3_sync.bucket                     = 'www.sicpdistilled.com'
-  s3_sync.region                     = 'eu-west-1'
-  s3_sync.delete                     = false
-  s3_sync.after_build                = true
-  s3_sync.prefer_gzip                = true
-  s3_sync.path_style                 = true
-  s3_sync.reduced_redundancy_storage = false
-  s3_sync.acl                        = 'public-read'
-  s3_sync.encryption                 = false
-  s3_sync.prefix                     = ''
-  s3_sync.version_bucket             = false
-end
+set :relative_links, true
+set :http_prefix, '/sicpdistilled'
 
 configure :build do
   activate :minify_css
@@ -42,30 +28,3 @@ configure :build do
   activate :asset_hash
 
 end
-
-activate :cdn do |cdn|
-  cdn.cloudflare = {
-    email: 'thattommyhall@gmail.com',
-    zone: 'sicpdistilled.com',
-    base_urls: [
-      'http://www.sicpdistilled.com'
-    ]
-  }
-end
-
-after_s3_sync do |files_by_status|
-  cdn_invalidate(files_by_status[:updated])
-end
-
-
-# ready do
-#   # proxy "/section/welcome", "/index.html"
-#   %w[0.1 1.1 1.2 2.1 2.2.4].each do |id|
-#     ['', '/'].each do |slash|
-#       proxy "/slides/#{id}#{slash}", "/presentation.html", layout: false do
-#         @id = id
-#         @content = File.read("slides/#{id}.md")
-#       end
-#     end
-#   end
-# end
